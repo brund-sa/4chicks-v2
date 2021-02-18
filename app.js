fb7a7339ebb5a33abf801691902e7cf42e15174e
Kevin <kevinbulfanisay1@gmail.com>
2021-02-18 18:47:54 +0300

    music randomized

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const audio_ = document.querySelector('#my_audio');
-const ck_1 = document.querySelector('.chickens1');
+const audio_2 = document.querySelector('#my_audio2');
 
+const ck_1 = document.querySelector('.chickens1');
+const ck_2 = document.querySelector('.chickens2');
+const ck_3 = document.querySelector('.chickens3');
+const ck_4 = document.querySelector('.chickens4');
 
 ck_1.addEventListener('click', ()=>{ 
     /* ck_1.style.display = 'none' */
     audio_.play();
+});
+
+ck_2.addEventListener('click', ()=>{ 
+    /* ck_1.style.display = 'none' */
+    audio_2.play();
+});
+
+ck_3.addEventListener('click', ()=>{ 
+    /* ck_1.style.display = 'none' */
+    audio_2.play();
+});
+
+ck_4.addEventListener('click', ()=>{ 
+    /* ck_1.style.display = 'none' */
+    audio_1.play();
 });
