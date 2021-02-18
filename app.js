fb768da0a3a79f3cbed43a6f2f6b520f1707e873
Kevin <kevinbulfanisay1@gmail.com>
2021-02-18 18:08:25 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const audio_ = document.querySelector('#my_audio');
+const ck_1 = document.querySelector('.chickens1');
+
+
+ck_1.addEventListener('click', ()=>{ 
+    /* ck_1.style.display = 'none' */
+    audio_.play();
+});
